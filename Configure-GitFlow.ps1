12d59f55231c583cea07f1530ce2a0fe53c59d6e
John Hoerr <jhoerr@iu.edu>
2014-08-28 10:30:03 -0400

    Support any version of PortableGit.

--- Configure-GitFlow.ps1
+++ Configure-GitFlow.ps1
@@ -1,33 +1,30 @@
 ﻿Function Configure-GitFlow
 {	
 	$currentPath = Split-Path ${function:Configure-GitFlow}.File -Parent
-	$operatingSystem = Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture
 
-	if ($operatingSystem.OSArchitecture -eq "64-bit")
-	{
-		$programFilesPath = ${Env:ProgramFiles(x86)}
-	}
-	else
-	{
-		$programFilesPath = ${Env:ProgramFiles}
-	}
+    # Find all 'PortableGit*' folders in the GitHub for Windows application folder
+	$gitHubPath = "C:\Users\$env:username\AppData\Local\GitHub"
+    $portableGitFolders = Get-ChildItem $gitHubPath | ?{ $_.PSIsContainer } | ?{$_.Name -like 'PortableGit*'}
+	
+    # Provision the PoSH GitFlow scripts for each instance of PortableGit
+    foreach ($portableGitFolder in $portableGitFolders){
 
-	$gitPath = "C:\Users\$env:username\AppData\Local\GitHub\PortableGit_6d98349f44ba975cf6c762a720f8259a267ea445\bin"
-	
-	Write-Host "Copying Required supporting binaries"
-	
-	Copy-Item (Join-Path $currentPath "Dependencies\*.*") -Destination $gitPath -Verbose
-	
-	Write-Host "Copying GitFlow extensions"
-	
-	Copy-Item (Join-Path $currentPath "GitFlowExtensions\**") -Destination $gitPath -Verbose
-	
-	Write-Host "Copying Git components"
-	
-	Copy-Item (Join-Path $gitPath "libiconv-2.dll") -Destination (Join-Path $gitPath "libiconv2.dll") -Verbose
+        $gitPath = (Join-Path (Join-Path $gitHubPath $portableGitFolder.Name) "bin")
+	    Write-Host "Installing extensions to Git binaries in '$gitPath'" -ForegroundColor Green
 
-	Write-Host "Press Any Key to finish"
-	Read-Host
+	    Write-Host "Copying Required supporting binaries"
+	    Copy-Item (Join-Path $currentPath "Dependencies\*.*") -Destination $gitPath -Verbose
+	
+	    Write-Host "Copying GitFlow extensions"
+	    Copy-Item (Join-Path $currentPath "GitFlowExtensions\**") -Destination $gitPath -Verbose
+	
+	    Write-Host "Copying Git components"
+	    Copy-Item (Join-Path $gitPath "libiconv-2.dll") -Destination (Join-Path $gitPath "libiconv2.dll") -Verbose
+    }
+
+    Write-Host "Press Any Key to finish"
+    Read-Host
 }
 
 Configure-GitFlow
+
