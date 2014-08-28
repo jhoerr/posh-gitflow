ff36af2d5ac68840ef65f274cd0a9a303083bd90
John Hoerr <jhoerr@iu.edu>
2014-08-28 09:59:26 -0400

    Import

---
+++ Configure-GitFlow.ps1
@@ -0,0 +1,33 @@
+﻿Function Configure-GitFlow
+{	
+	$currentPath = Split-Path ${function:Configure-GitFlow}.File -Parent
+	$operatingSystem = Get-WmiObject -Class Win32_OperatingSystem | Select-Object OSArchitecture
+
+	if ($operatingSystem.OSArchitecture -eq "64-bit")
+	{
+		$programFilesPath = ${Env:ProgramFiles(x86)}
+	}
+	else
+	{
+		$programFilesPath = ${Env:ProgramFiles}
+	}
+
+	$gitPath = "C:\Users\$env:username\AppData\Local\GitHub\PortableGit_6d98349f44ba975cf6c762a720f8259a267ea445\bin"
+	
+	Write-Host "Copying Required supporting binaries"
+	
+	Copy-Item (Join-Path $currentPath "Dependencies\*.*") -Destination $gitPath -Verbose
+	
+	Write-Host "Copying GitFlow extensions"
+	
+	Copy-Item (Join-Path $currentPath "GitFlowExtensions\**") -Destination $gitPath -Verbose
+	
+	Write-Host "Copying Git components"
+	
+	Copy-Item (Join-Path $gitPath "libiconv-2.dll") -Destination (Join-Path $gitPath "libiconv2.dll") -Verbose
+
+	Write-Host "Press Any Key to finish"
+	Read-Host
+}
+
+Configure-GitFlow
---
+++ README.md
@@ -0,0 +1,12 @@
+posh-gitflow
+============
+
+PowerShell extensions to enable Git Flow-based source code management
+
+**Installation**
+
+1. Clone this repository on your development machine
+2. In the GitHub for Windows client, browse to the repository and 'Open in Git Shell'
+3. Run `./Configure-GitFlow.ps1` at the prompt
+
+
