0d30c870facffbdf7107c83a950a10d7f9819012
Adrian Padilla Vega <adpadi@microsoft.com>
2017-07-18 14:44:28 -0700

    Adding switch parameter to configuration script to allow silent installation

--- Configure-GitFlow.ps1
+++ Configure-GitFlow.ps1
@@ -1,4 +1,10 @@
-﻿<# 
+﻿param
+(
+[Alias("Silent")]
+	[switch] $SilentInstallation
+)
+
+<# 
 .SYNOPSIS 
     Adds PowerShell GitFlow extensions to GitHub for Windows
 .DESCRIPTION
@@ -6,6 +12,9 @@
     by nvie (https://github.com/nvie/gitflow). This script is itself a modified version of a script developed 
     by Howard van Rooijen and documented in a fantastic series of blog posts about using GitHub with TeamCity: 
     http://blogs.endjin.com/2013/03/a-step-by-step-guide-to-using-gitflow-with-teamcity-part-1-different-branching-models/
+.PARAMETER SilentInstallation
+Optional installation with no prompts to the user.
+
 .NOTES 
     Author     : John Hoerr - jhoerr@gmail.com
 .LINK 
@@ -54,8 +63,11 @@ Function Configure-GitFlow
 		Copy-Item (Join-Path $gitMingW64BinPath "libiconv-2.dll") -Destination (Join-Path $gitBinPath "libiconv2.dll") -Verbose
     }
 
-	Write-Host "Press Enter to finish"
-	Read-Host
+    if(!$SilentInstallation)
+    {
+        Write-Host "Press Enter to finish"
+	    Read-Host
+    }
 }
 
 Configure-GitFlow
